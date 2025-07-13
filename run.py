6378fe934a0925da14accd08b78a93304a268d46
tsukiram <weebdur@gmail.com>
2025-07-13 22:51:06 +0700

    sda

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # run.py
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default ke 5000 jika PORT tidak ada
+    app.run(host='0.0.0.0', port=port, debug=True)
